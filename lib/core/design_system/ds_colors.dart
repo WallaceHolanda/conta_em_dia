22f38df0f2309e6c05c7c5a1e845ec3612e80f36
wallaceholanda <wallace.holanda@hotmail.com>
2026-01-20 20:35:57 -0300

    feat: adiciona arquivo de cores

---
+++ lib/core/design_system/ds_colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class DsColors {
+  DsColors._();
+
+  // Primary
+  static const Color primary = Color(0xFF1E40AF);
+  static const Color primary10 = Color(0xFFEDF1FF);
+  static const Color primary20 = Color(0xFFB7C8FE);
+  static const Color primary30 = Color(0xFF829FFE);
+  static const Color primary40 = Color(0xFF4971F3);
+  static const Color primary60 = Color(0xFF041C6B);
+
+  // Neutral
+  static const Color neutral10 = Color(0xFFEEEEEE);
+  static const Color neutral20 = Color(0xFFCCCCCC);
+  static const Color neutral30 = Color(0xFFAAAAAA);
+  static const Color neutral40 = Color(0xFF888888);
+  static const Color neutral50 = Color(0xFF666666);
+  static const Color neutral60 = Color(0xFF444444);
+
+  // Alert
+  static const Color info = Color(0xFFF59E0B);
+  static const Color error = Color(0xFFDC2626);
+  static const Color success = Color(0xFF16A34A);
+
+  static const Color black = Color(0xFF000000);
+  static const Color background = Color(0xFFF1F1F1);
+  static const Color whiteBackground = Color(0xFFFCFCFC);
+}
