7df8fbfff419e9141cff88435c481af5756b20cb
wallaceholanda <wallace.holanda@hotmail.com>
2026-01-21 19:08:19 -0300

    feat: adiciona imagens de banco e cartoes

--- lib/core/assets/ds_images_enum.dart
+++ lib/core/assets/ds_images_enum.dart
@@ -1,7 +1,18 @@
 enum DsImagesEnum {
   logo('assets/images/logo.svg'),
   logoAlt('assets/images/logo_alt.svg'),
-  onboardingImage('assets/images/onboarding.png')
+  onboardingImage('assets/images/onboarding.png'),
+  shortLogo('assets/images/short_logo.svg'),
+  bradesco('assets/images/bank/bradesco.svg'),
+  caixa('assets/images/bank/caixa.svg'),
+  santander('assets/images/bank/santander.svg'),
+  caju('assets/images/card/caju.svg'),
+  nubank('assets/images/card/nubank.svg'),
+  alelo('assets/images/card/alelo.svg'),
+  elo('assets/images/network/elo.svg'),
+  hipercard('assets/images/network/hipercard.svg'),
+  mastercard('assets/images/network/mastercard.svg'),
+  visa('assets/images/network/visa.svg'),
   ;
 
   final String path;
